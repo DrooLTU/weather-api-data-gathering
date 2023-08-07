a02b104c9765767d23e1f810a845e4ad3e194e0c
DrooLTU <dzastinaslt@gmail.com>
2023-08-07 16:15:01 +0300

    Initial City CRUD controller created

---
+++ src/crud/city_crud.py
@@ -0,0 +1,26 @@
+import os
+import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+from sqlalchemy.orm import Session
+from schemas.city_schema import CitySchema
+
+def get_city(db: Session, city: int|str) -> CitySchema:
+    match city:
+        case int():
+            return db.query(CitySchema).filter(CitySchema.id == city).first()
+        case str():
+            return db.query(CitySchema).filter(CitySchema.city == city).first()
+
+
+def get_cities(db: Session, skip: int = 0, limit: int = 100) -> list[CitySchema]:
+    return db.query(CitySchema).offset(skip).limit(limit).all()
+
+
+def create_city(db: Session, city: CitySchema) -> CitySchema:
+    db_city = CitySchema(**city.model_dump())
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
